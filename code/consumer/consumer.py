9b61991426db03d21c359e0e39f6bf8d1893c6f4
YakirShriker <38837459+YakirShriker@users.noreply.github.com>
2020-09-08 22:45:49 +0300

    Update consumer.py

--- code/consumer/consumer.py
+++ code/consumer/consumer.py
@@ -1,29 +1,45 @@
 import pika, logging, sys, argparse, time
 from argparse import RawTextHelpFormatter
 from time import sleep
+from prometheus_client import start_http_server,Gauge
+
+
+global counter
+counter = 0
 
 def on_message(channel, method_frame, header_frame, body):
-    print method_frame.delivery_tag
-    print body
+    print (method_frame.delivery_tag)
+    print (body)
     print
     LOG.info('Message has been received %s', body)
+    counter+=1
+    consumer_messages_count.set(counter)
+
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+
+def prometheus_register():
+    """ Register the metrics for Prometheus.
+    Args : None
+    Return : None
+    """
+    global consumer_messages_count
+    consumer_messages_count = Gauge('consumer_messages_count', 'Consumer Messages count')
 
 
 if __name__ == '__main__':
     examples = sys.argv[0] + " -p 5672 -s rabbitmq "
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
-                                 description='Run consumer.py',
-                                 epilog=examples)
+                                     description='Run consumer.py',
+                                     epilog=examples)
     parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.')
     parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.')
 
     args = parser.parse_args()
     if args.port == None:
-        print "Missing required argument: -p/--port"
+        print ("Missing required argument: -p/--port")
         sys.exit(1)
     if args.server == None:
-        print "Missing required argument: -s/--server"
+        print ("Missing required argument: -s/--server")
         sys.exit(1)
 
     # sleep a few seconds to allow RabbitMQ server to come up
@@ -36,7 +52,13 @@ if __name__ == '__main__':
                                            '/',
                                            credentials)
     connection = pika.BlockingConnection(parameters)
+
+    print('Starting Prometheus web-server')
+    start_http_server(9422)
+    print('Registering metrics to Prometheus')
+    prometheus_register()
     channel = connection.channel()
+
 
     channel.queue_declare('pc')
     channel.basic_consume(on_message, 'pc')
